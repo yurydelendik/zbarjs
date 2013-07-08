284135a673d47273ed29f66d970cf35c5fb0c5fa
notmasteryet <async.processingjs@yahoo.com>
2013-07-08 17:05:33 -0500

    building gh-pages

---
+++ build-gh-pages.sh
@@ -0,0 +1,22 @@
+#/bin/bash.sh
+rm -rf gh-pages
+mkdir gh-pages
+
+cp -R \
+index.html \
+zbar-main.js \
+zbar-processor.js \
+icon-128.png \
+LICENSE \
+cache.manifest \
+manifest.webapp \
+css \
+gh-pages/
+
+cd gh-pages
+git init
+git checkout -b gh-pages
+git add *
+git commit -m "created by build-gh-pages.sh"
+
+echo "cd gh-pages; git push https://github.com/<username>/zbarjs.git gh-pages"
