9c35ce5c1fcfc0237360c855179cc5365c63f492
André Fiedler <andre.fiedler@me.com>
2014-06-02 10:53:00 +0200

    implemented live view scanning

--- zbar-processor.js
+++ zbar-processor.js
@@ -2,6 +2,7 @@
 // Compiled from zbar.sf.net using emscripten.
 //
 // Copyright (C) 2013 Yury Delendik
+// Modified 2014, André Fiedler
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -15,21 +16,43 @@
 //
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
-function zbarProcessImageData(imgData) {
+self.onmessage = function(e) {
+    var result = zbarProcessImageData(e.data.imageData, e.data.width, e.data.height);
+    if (result.length > 0) {
+        postMessage({
+            result: result,
+            success: true
+        });
+    } else {
+        postMessage({
+            result: [],
+            success: false
+        });
+    }
+}
+
+function Log(message) {
+    postMessage({
+        result: message,
+        success: 'log'
+    });
+}
+
+function zbarProcessImageData(imgData, width, height) {
   var result = [];
   var Module = {};
-  Module['imageWidth'] = imgData.width;
-  Module['imageHeight'] = imgData.height;
-  Module['getImageData'] = function (grayData) {
-    var d = imgData.data;
-    for (var i = 0, j = 0; i < d.length; i += 4, j++) {
-      grayData[j] = (d[i] * 66 + d[i + 1] * 129 + d[i + 2] * 25 + 4096) >> 8;
-    }
+  Module.imageWidth = width;
+  Module.imageHeight = height;
+  Module.getImageData = function(grayData) {
+      var d = imgData;
+      for (var i = 0, j = 0; i < d.length; i += 4, j++) {
+          grayData[j] = (d[i] * 66 + d[i + 1] * 129 + d[i + 2] * 25 + 4096) >> 8;
+      }
   };
-  Module['outputResult'] = function (symbol, addon, data) {
-    result.push([symbol, addon, data]);
+  Module.outputResult = function(symbol, addon, data) {
+      result.push([symbol, addon, data]);
   };
 
   /* EMSCRIPTEN_CODE */
